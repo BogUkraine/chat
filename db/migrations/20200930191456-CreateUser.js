ff891b6ce5402038c29fe7a56df22cd85bc8a55e
BogUkraine <BogUkraine@gmail.com>
2020-10-01 23:24:14 +0300

    fine sequelize structure

--- db/migrations/20200930191456-CreateUser.js
+++ db/migrations/20200930191456-CreateUser.js
@@ -1,10 +1,9 @@
 'use strict';
-const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.createTable('users', {
-      id   : { type : Sequelize.UUID, allowNull : false, primaryKey : true},
+      id   : { type : Sequelize.UUID, defaultValue: Sequelize.fn('uuid_generate_v4'), primaryKey : true},
       name : { type : Sequelize.STRING, allowNull : false},
     });
   },
--- db/migrations/20200930192042-CreateMessage.js
+++ db/migrations/20200930192042-CreateMessage.js
@@ -1,10 +1,9 @@
 'use strict';
-const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.createTable('messages', {
-      id         : { type : Sequelize.UUID, allowNull : false, primaryKey : true},
+      id         : { type : Sequelize.UUID, defaultValue: Sequelize.fn('uuid_generate_v4'), primaryKey : true},
       value      : { type : Sequelize.STRING, allowNull : false},
       created_by : { type : Sequelize.UUID, allowNull : false},
       chat_id    : { type : Sequelize.UUID, allowNull : false},
