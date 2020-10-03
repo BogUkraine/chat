b27a979b64d1d8798b40e3f60cf9752bf7610901
BogUkraine <BogUkraine@gmail.com>
2020-10-03 17:32:39 +0300

    react send message

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Home from './components/Home';
+import ApolloProvider from './ApolloProvider'
 
 const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <ApolloProvider>
+      <div className="App">
+        <Home />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,40 @@
-import React, { useRef } from 'react';
+import React, { useRef, createContext, useReducer, useContext } from 'react';
+import { gql, useMutation, useLazyQuery } from '@apollo/client';
+
+const SEND_MESSAGE = gql`
+  mutation sendMessage($value: String!, $created_by: ID!) {
+    createMessage(messageCreateInput : {value: $value, created_by: $created_by}) {
+      created_by
+      value
+    }
+  }
+`
+
+const GET_MESSAGES = gql`
+  query getMessages($from: String!) {
+    getMessages(from: $from) {
+        id
+        created_by
+        chat_id
+        value
+        created_at
+    }
+  }
+`
 
 const Home = () => {
     const inputRef = useRef();
 
-    const sendMessage = (event) => {
+    //const [ getMessages, { loading: messagesLoading, data: messagesData } ] = useLazyQuery(GET_MESSAGES)
+
+    const [sendMessage] = useMutation(SEND_MESSAGE, {
+        onError: (err) => console.log(err),
+    })
+
+    const submitMessage = (event) => {
         event.preventDefault();
-        console.log(inputRef.current.value);
-    }
+        sendMessage({ variables: { value : inputRef.current.value, created_by : '24696d93-cab1-497c-9ba0-c55011bab0b5'}})
+    }    
 
     return (
         <div className="home">
@@ -15,7 +43,7 @@ const Home = () => {
             </div>
             <div className="home__form">
                 <input type="text" ref={inputRef}/>
-                <button onClick={sendMessage}>Send</button>
+                <button onClick={submitMessage}>Send</button>
             </div>
         </div>
     );
